0c267dfc248fe340c2725a305cf6e2f4d076bdd2
rengganiks <143510198+rengganiks@users.noreply.github.com>
2024-11-25 21:33:45 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-// import node from '@astrojs/node';
+import node from '@astrojs/node';
 
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 import icon from "astro-icon";
 // // default
@@ -14,18 +14,18 @@ import icon from "astro-icon";
 // });
 // vercel
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
-  output: 'server',
-  adapter: vercel(),
-  outDir:'./dist'
-});
-// vps
 // export default defineConfig({
-//   output: 'server',
 //   integrations: [react(), tailwind(), icon()],
-//   adapter: node({
-//     mode: 'standalone',
-//   }),
+//   output: 'server',
+//   adapter: vercel(),
 //   outDir:'./dist'
 // });
+// vps
+export default defineConfig({
+  output: 'server',
+  integrations: [react(), tailwind(), icon()],
+  adapter: node({
+    mode: 'standalone',
+  }),
+  outDir:'./dist'
+});
