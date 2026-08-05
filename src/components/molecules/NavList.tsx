8c6953ad2b44332dbb64615b2e309e2623d48f87
ACH RIZAL <achrizal15@gmail.com>
2026-08-05 08:58:24 +0700

    feat: create NavList component for dynamic site navigation menu

--- src/components/molecules/NavList.tsx
+++ src/components/molecules/NavList.tsx
@@ -2,18 +2,29 @@ import NavLink from '@components/atoms/NavLink';
 import React from 'react';
 
 interface NavListProps {
-  isMobile?: boolean
-  currentPathName: string
-  isSolid?: boolean
+  isMobile?: boolean;
+  currentPathName: string;
+  isSolid?: boolean;
 }
 
-const NavList: React.FC<NavListProps> = ({ isMobile = false, currentPathName, isSolid = false }) => (
-  <ul className={`${isMobile ? 'flex flex-col justify-start gap-10' : 'lg:flex items-center hidden'} xl:gap-7  lg:gap-5 font-roboto`}>
-    <NavLink scrolledDefault={isMobile || isSolid} href="/" isActive={currentPathName == '/'}>Beranda</NavLink>
-    <NavLink scrolledDefault={isMobile || isSolid} href="/services" isActive={currentPathName == '/services'}>Layanan</NavLink>
-    <NavLink scrolledDefault={isMobile || isSolid} href="/blogs" isActive={currentPathName == '/blogs' || currentPathName.startsWith('/blogs')}>Blog</NavLink>
-    <NavLink scrolledDefault={isMobile || isSolid} href="/bukubatik" isActive={currentPathName == '/bukubatik' || currentPathName.startsWith('/bukubatik')} isHighlight={true}>BukuBatik</NavLink>
-  </ul>
-);
+const NavList: React.FC<NavListProps> = ({ isMobile = false, currentPathName = '', isSolid = false }) => {
+  const cleanPath = currentPathName.endsWith('/') && currentPathName.length > 1
+    ? currentPathName.slice(0, -1)
+    : currentPathName;
+
+  const isHomeActive = cleanPath === '' || cleanPath === '/';
+  const isServicesActive = cleanPath === '/services' || cleanPath.startsWith('/services/');
+  const isBlogsActive = cleanPath === '/blogs' || cleanPath.startsWith('/blogs/');
+  const isBukuBatikActive = cleanPath === '/bukubatik' || cleanPath.startsWith('/bukubatik/');
+
+  return (
+    <ul className={`${isMobile ? 'flex flex-col justify-start gap-10' : 'lg:flex items-center hidden'} xl:gap-7 lg:gap-5 font-roboto`}>
+      <NavLink scrolledDefault={isMobile || isSolid} href="/" isActive={isHomeActive}>Beranda</NavLink>
+      <NavLink scrolledDefault={isMobile || isSolid} href="/services" isActive={isServicesActive}>Layanan</NavLink>
+      <NavLink scrolledDefault={isMobile || isSolid} href="/blogs" isActive={isBlogsActive}>Blog</NavLink>
+      <NavLink scrolledDefault={isMobile || isSolid} href="/bukubatik" isActive={isBukuBatikActive} isHighlight={true}>BukuBatik</NavLink>
+    </ul>
+  );
+};
 
 export default NavList;
